57b07e86986abd3379e61c994f6f2ea59adff8f3
Daniel Luna <dancluna@gmail.com>
2013-02-11 16:30:34 -0200

    added sandbox-defun and sandbox-while from erbot

--- spec/sandbox-spec.el
+++ spec/sandbox-spec.el
@@ -3,15 +3,15 @@
 (describe "sandbox"
   (it "returns nil when passed the empty list"
     (should (null (sandbox '()))))
-  
+
   (it "rewrites any function as a function with a prefix"
     (should (equal (sandbox '(hi t))
                    '(emacs-sandbox-hi t))))
-  
+
   (it "allows t, nil, &rest, &optional..."
     (should (equal (sandbox '(t nil &rest &optional))
                    '(t nil &rest &optional))))
-  
+
   (it "passes a quoted form along as quoted"
     ;; should this be a preference?
     (should (equal (sandbox ''(hi t))
@@ -20,10 +20,10 @@
 (describe "sandbox--check-args"
   (it "is true for an empty list"
     (should (equal t (sandbox--check-args nil))))
-  
+
   (it "is true for a list with a single symbol"
     (should (equal t (sandbox--check-args '(wtf)))))
-  
+
   (it "is true for a list with multiple symbols"
     (should (equal t (sandbox--check-args '(wtf omg)))))
 
@@ -43,22 +43,28 @@
     (should (sandbox--safe-length-args-p '((2 3) 2 5 6 7 ) 0 4))
     (should-not (sandbox--safe-length-args-p '((2 3) 2) 0 2))))
 
+(describe "sandbox-defun"
+  (it "makes "))
 
-
+(describe "sandbox-while"
+  (it "wont allow infinite looping"
+    (should-error
+     (let ((i 0))
+       (sandbox-while t (incf i))))))
 
 ;; actual spec tests
 (describe "sandbox"
   (it "forbids the user from executing the bad stuff"
-    (with-mock2 
+    (with-mock2
       (defmock a-sensitive-function ())
-      (should-error 
+      (should-error
        (eval (sandbox '(a-sensitive-function)))
-       
+
        :type 'void-function)
       (should (= 0 (el-spec:called-count 'a-sensitive-function)))))
-  
+
   (it "allows the user to execute the good stuff"
-    (with-mock2 
+    (with-mock2
       (defmock emacs-sandbox-not-sensitive ())
       (eval (sandbox '(not-sensitive)))
                 (should (= 1 (el-spec:called-count 'emacs-sandbox-not-sensitive))))))
@@ -66,7 +72,7 @@
 
 (describe "an infinite loop condition"
   (it "cant allow looping, i guess"
-    (should-error 
+    (should-error
      (eval (sandbox '(while t
                        (throw 'omg-should-not-even-be-allowed-to-run!!!)))))
     :type 'void-function))
@@ -77,9 +83,9 @@
 (describe "user trying to access an outside variable"
   (it "doesnt work"
     (let ((a-secret 'shhhhh))
-      (should-error 
+      (should-error
        (eval (sandbox '(message a-secret)))
-       
+
        :type 'void-variable
        )
 
@@ -88,5 +94,5 @@
 
 ;;;;;;;;;;;;;;;;
 ;; need two types of specs to advance:
-;; the first is examples of 
+;; the first is examples of
 ;;
