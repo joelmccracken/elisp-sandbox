eb92e53fbd6c9e7a7dc22b72dcffa047d6f7e3be
Joel McCracken <mccracken.joel@gmail.com>
2013-04-21 15:26:27 -0400

    heavy modification of the test infrastructure

---
+++ spec/spec-helpers.el
@@ -0,0 +1,29 @@
+
+(defvar sandbox-test-run-on-eval nil "should evaling a test definition run it immediately")
+(setq sandbox-test-run-on-eval nil)
+(defun sandbox-test--toggle-run-on-eval ()
+  (interactive)
+  (setq sandbox-test-run-on-eval (not sandbox-test-run-on-eval)))
+
+(defun sandbox--testing-symbol-from-string (str)
+  (intern (replace-regexp-in-string "[^a-z0-9]" "-" str)))
+
+
+(defmacro sandbox-test (docstring &rest body)
+  (declare (indent defun))
+  (let* ((docstring (eval docstring))
+         (test-name (sandbox--testing-symbol-from-string docstring)))
+    `(let (the-test
+           sandbox-after-test-hook)
+       (setq the-test
+             (ert-deftest ,test-name ()
+               ,docstring
+               :tags '(sandbox)
+               (unwind-protect
+                   ,@body
+                 (run-hooks 'sandbox-after-test-hook))))
+       (when sandbox-test-run-on-eval
+         (ert-run-tests-interactively the-test))
+       the-test)))
+
+
